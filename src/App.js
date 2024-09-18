483338da09462852fd4e11ba4c9df6a17685b6b1
nayeamseikhh <usanayeamseikh@gmail.com>
2024-09-18 08:08:52 +0600

    4

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, {useState, useEffect } from 'react';
 import './App.css';
 import Country from './Components/country/Country';
+import Cart from './Cart/Cart';
 
-const handleAddCountry = (country) => {
-  console.log('country added', country)} 
+
 // button event-Handler diclear Here 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(()=> {
     fetch('https://restcountries.com/v3.1/all')
     .then(res => res.json())
     .then(data=> {setCountries(data);})
   }, [])
+
+  const handleAddCountry = (country) => {
+    const newCart = [...cart, country];
+    setCart(newCart);
+  }
+     
+
   return (
     <div className="App">
      <h1>Country: {countries.length}</h1>
+     <h4>Cart Added: {cart.length}</h4>
+     <Cart cart = {cart} ></Cart>
      {
       countries.map(country=> <Country country= {country} handleAddCountry={handleAddCountry} key={country.name.common}></Country>)
       //* data/parametar send=      handleAddCountry={handleAddCountry}
